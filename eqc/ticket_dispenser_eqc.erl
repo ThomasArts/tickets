727c9649cf5dd98aa7de9f4a381e8145bc1b2bc9
Thomas Arts <thomas.arts@quviq.com>
2016-01-25 10:19:41 +0100

    Make HTTPC wrapper to enforce new profiles per process

--- eqc/ticket_dispenser_eqc.erl
+++ eqc/ticket_dispenser_eqc.erl
@@ -5,6 +5,7 @@
 
 -compile(export_all).
 
+
 initial_state() ->
   1.
 
@@ -26,8 +27,6 @@ take() ->
   Txt = http(get, "http://localhost:4000/take"),
   list_to_integer(Txt).
 
-
-
 take_next(State, _Result, []) ->
   State + 1.
 
@@ -41,18 +40,25 @@ weight(_State, reset) ->
   1.
 
 prop_par_ticket_dispenser() ->
-  inets:start(),
   with_parameter(print_counterexample, false,
   ?FORALL(Cmds, parallel_commands(?MODULE),
+  ?ALWAYS(10,
     begin
       reset(),
       {History, State, Result} = run_parallel_commands(?MODULE, Cmds),
       pretty_commands(?MODULE, Cmds, {History, State, Result},
           aggregate(command_names(Cmds),
             Result == ok))
-    end)).
+    end))).
 
 http(get, URL) ->
-  {ok, {{"HTTP/1.1", 200, "OK"}, _, Txt}} = httpc:request(get, {URL, []}, [], []),
-  % timer:sleep(600),
+  Profile = 
+    case get(profile) of
+      undefined -> 
+        P = list_to_atom("eqc_parallel_pid_"++ pid_to_list(self())),
+        inets:start(httpc, [{profile, P}]),
+        put(profile, P), P;
+      Prof -> Prof
+    end,
+  {ok, {{"HTTP/1.1", 200, "OK"}, _, Txt}} = httpc:request(get, {URL, []}, [], [], Profile),
   Txt.
